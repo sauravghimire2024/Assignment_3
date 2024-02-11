0fac5559fdcfc6a1e642a3df67b50a90fe809146
krishnasubedi0603 <k.subedi0603@gmail.com>
2024-02-11 11:41:49 +0900

    question_2 create saw class

--- game.py
+++ game.py
@@ -84,3 +84,32 @@ class player(object):
             self.hitbox = (self.x + 4, self.y, self.width-24, self.height-13)
 
         # pygame.draw.rect(win, (255,0,0),self.hitbox, 2)
+
+
+class saw(object):
+    rotate = [pygame.image.load(os.path.join('images', 'SAW0.PNG')), pygame.image.load(os.path.join(
+        'images', 'SAW1.PNG')), pygame.image.load(os.path.join('images', 'SAW2.PNG')), pygame.image.load(os.path.join('images', 'SAW3.PNG'))]
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rotateCount = 0
+        self.vel = 1.4
+
+    def draw(self, win):
+        self.hitbox = (self.x + 10, self.y + 5,
+                       self.width - 20, self.height - 5)
+        # pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
+        if self.rotateCount >= 8:
+            self.rotateCount = 0
+        win.blit(pygame.transform.scale(
+            self.rotate[self.rotateCount//2], (64, 64)), (self.x, self.y))
+        self.rotateCount += 1
+
+    def collide(self, rect):
+        if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
+            if rect[1] + rect[3] > self.hitbox[1]:
+                return True
+        return False
